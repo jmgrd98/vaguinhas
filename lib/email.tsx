0603bfe1c5bc68ee7f44537af9e19c06e045a66e
jmgrd98 <jmgrd98@gmail.com>
2025-07-18 15:22:35 -0300

    temporary hotfix - use gmail smtp instead of brevo smtp

--- lib/email.tsx
+++ lib/email.tsx
@@ -26,17 +26,21 @@ export interface Job {
 }
 
 const transporter = createTransport({
-  // service: process.env.EMAIL_SERVICE,
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
-  secure: process.env.SMTP_SECURE === 'true',
+  service: process.env.EMAIL_SERVICE,
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASSWORD,
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
   },
-  tls: {
-    ciphers: 'SSLv3'
-  }
+  // host: process.env.SMTP_HOST,
+  // port: Number(process.env.SMTP_PORT),
+  // secure: process.env.SMTP_SECURE === 'true',
+  // auth: {
+  //   user: process.env.SMTP_USER,
+  //   pass: process.env.SMTP_PASSWORD,
+  // },
+  // tls: {
+  //   ciphers: 'SSLv3'
+  // }
 
 });
 
