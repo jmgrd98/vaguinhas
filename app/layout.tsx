13f809bc4c64653ab1028b20787014c67d229be4
jmgrd98 <jmgrd98@gmail.com>
2025-08-04 18:35:02 -0300

    fix build errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,33 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Caprasimo } from "next/font/google";
+// import type { Metadata } from "next";
+// import { Geist, Geist_Mono, Caprasimo } from "next/font/google";
 import "./globals.css";
 import Providers from "@/app/Providers";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/lib/googleAuth";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
-const caprasimo = Caprasimo({
-  weight: "400",
-  variable: "--font-caprasimo",
-  subsets: ["latin"],
-});
+// const caprasimo = Caprasimo({
+//   weight: "400",
+//   variable: "--font-caprasimo",
+//   subsets: ["latin"],
+// });
 
-export const metadata: Metadata = {
-  title: "vaguinhas",
-  description: "Receba vagas personalizadas de tecnologia diariamente no seu e-mail.",
-  icons: {
-    icon: '/favicon.ico'
-  }
-};
+// export const metadata: Metadata = {
+//   title: "vaguinhas",
+//   description: "Receba vagas personalizadas de tecnologia diariamente no seu e-mail.",
+//   icons: {
+//     icon: '/favicon.ico'
+//   }
+// };
 
 export default async function RootLayout({
   children,
@@ -38,7 +38,8 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} ${caprasimo.variable} antialiased`}>
+      {/* <body className={`${geistSans.variable} ${geistMono.variable} ${caprasimo.variable} antialiased`}> */}
+      <body className={` antialiased`}>
         <Providers session={session}>
           {children}
         </Providers>
--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,13 @@ import * as webpack from 'webpack';
 const nextConfig: NextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['ioredis'],
+    forceSwcTransforms: true,
+  },
+  compiler: {
+    styledComponents: true,
   },
    webpack: (config) => {
-    // Load environment variables for server-side
+    config.resolve.fallback = { fs: false, path: false };
     config.plugins.push(new webpack.EnvironmentPlugin(process.env));
     return config;
   },
